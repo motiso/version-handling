9ab9654f77abf59db14eccb9ec9ae012e93452e0
moti <moti@wajam.com>
2017-10-18 22:36:01 +0300

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,1 +1,26 @@
 
+@NonCPS
+def jobStartedByWhat() {
+def startedByWhat = ''
+try {
+    def buildCauses = currentBuild.rawBuild.getCauses()
+    for ( buildCause in buildCauses ) {
+        if (buildCause != null) {
+            def causeDescription = buildCause.getShortDescription()
+            echo "shortDescription: ${causeDescription}"
+            if (causeDescription.contains("Started by timer")) {
+                startedByWhat = 'timer'
+            }
+            if (causeDescription.contains("Started by user")) {
+                startedByWhat = 'user'
+            }
+        }
+    }
+} catch(theError) {
+    echo "Error getting build cause: ${theError}"
+}
+
+return startedByWhat
+}
+
+return this;
